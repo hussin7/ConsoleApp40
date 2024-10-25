b827836ca13fbcf0fc9899cc597138e5e54229ee
Hussein Salim Aziz <xx11zz33mn@gmail.com>
2024-10-25 16:06:49 +0300

    Initial commit

---
+++ ConsoleApp40/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    internal class Queue
+    {
+        int front;
+        int rear;
+        int size;
+        int[] qearr;
+        public Queue(int len)
+        {
+            size = len;
+            front = -1;
+            rear = -1;
+            qearr = new int[size];
+        }
+        public bool ISEmtay()
+        {
+            return rear== - 1;
+        }
+        public bool IsFull()
+        {
+            return rear==size-1;
+        }
+        public void Enqeueu(int element)
+        {
+            qearr[++rear]=element;
+            if (front == -1)
+                front++;
+        }
+        public int Dequeue()
+        {
+            int x = qearr[front];
+            if (front == rear)
+            {
+                front = -1;
+                rear = -1;
+            }
+            else
+                front++;
+            return x;
+        }
+        public void Display()
+        {
+            Console.WriteLine("The elements of Queue is");
+            for (int i = front;i<=rear;i++)
+            {
+                Console.WriteLine(qearr[i]);
+            }
+        }
+    }
+}
---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of queue:");
+            int len=int.Parse(Console.ReadLine());
+            Queue que = new Queue(len);
+            Queue queOdd=new Queue(len);
+
+            for(int i = 0; i < len; i++)
+            {
+                if (que.IsFull())
+                {
+                    Console.WriteLine("Queue is Full");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter the element:");
+                    int x = int.Parse(Console.ReadLine());
+                    que.Enqeueu(x);
+                }
+            }
+            que.Display();
+
+            for(int i = 0; i < len; i++)
+            {
+                int elim = que.Dequeue();
+                if (elim % 2 != 0)
+                {
+                    queOdd.Enqeueu(elim);
+                }
+            }           
+            queOdd.Display();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
